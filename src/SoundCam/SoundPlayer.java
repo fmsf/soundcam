65cccb6fcc51773cba7e8ca92d8d569c3a4fb0f4
José Miguel Malaca <goldenye@hotmail.com>
2014-04-11 16:30:39 +0100

    Update on comments in SoundPlayer

--- src/SoundCam/SoundPlayer.java
+++ src/SoundCam/SoundPlayer.java
@@ -22,10 +22,10 @@ public class SoundPlayer {
         if ( note != currentNote ) {
           Mp3Instrument mi = new Mp3Instrument( this.minim , this.path);
           
-          //default values for piano mp3 sounds, DO to RE [0, 4, 6, 12, 19, 22, 26]
+          //default values for piano mp3 sounds, DO to RE [0, 4, 6, 12, 19, 22, 26], Available: 30 sounds
           mi.playPiano(note, new int[]{0, 4, 6, 12, 19, 22, 26});
           
-          //default values for guitar mp3 sounds, DO to RE [8, 1, 2, 3, 4, 5, 6]
+          //default values for guitar mp3 sounds, DO to RE [8, 1, 2, 3, 4, 5, 6], Available: 10 sounds
           mi.playGuitar(note, new int[]{8, 1, 2, 3, 4, 5, 6});
           
           output.unmute();
